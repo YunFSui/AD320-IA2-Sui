3b8200cc5d10f4252f09089b0c56ee75381463bc
YunFSui <syatwork@outlook.com>
2019-01-21 21:01:20 -0800

    format fix to homework standard

--- welcome.php
+++ welcome.php
@@ -5,16 +5,16 @@
 
     <body>
 
-    Welcome <?php echo $_GET["name"]; ?><br>
-    Your email is: <?php echo $_GET["email"]; ?><br>
+    Name: <?php echo $_GET["name"]; ?><br>
+    Email: <?php echo $_GET["email"]; ?><br>
     <?php
     if ($_GET["phoneNumber"] != "") {
-        echo "Your Phone Number is " . $_GET["phoneNumber"];
+        echo "Phone: " . $_GET["phoneNumber"];
     }
     ?><br>
     <?php
     if ($_GET["stateOfResidence"] != "") {
-        echo "You state of residence is " . $_GET["stateOfResidence"];
+        echo "State: " . $_GET["stateOfResidence"];
     }
     ?>
 
