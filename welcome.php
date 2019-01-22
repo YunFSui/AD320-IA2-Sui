d29214c5841b578fdff238496e0ee990089017e0
YunFSui <syatwork@outlook.com>
2019-01-21 20:57:11 -0800

    issue fixs now works correctly

--- welcome.php
+++ welcome.php
@@ -1,21 +1,20 @@
+<!DOCTYPE html>
 <html>
     <head></head>
 
-    
+
     <body>
 
     Welcome <?php echo $_GET["name"]; ?><br>
     Your email is: <?php echo $_GET["email"]; ?><br>
     <?php
-    $x = isset($_GET["phoneNumber"]);
-    if ($x) {
-        echo "Your Phone Number is " + $_GET["phoneNumber"];
+    if ($_GET["phoneNumber"] != "") {
+        echo "Your Phone Number is " . $_GET["phoneNumber"];
     }
     ?><br>
     <?php
-    $y = isset($_GET["stateOfResidence"]);
-    if ($y) {
-        echo "You state of residence is " + $_GET["stateOfResidence"];
+    if ($_GET["stateOfResidence"] != "") {
+        echo "You state of residence is " . $_GET["stateOfResidence"];
     }
     ?>
 
